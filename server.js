5bf37b384f09bbd5f236dd61048e6dc8d8289c68
Tara Kreft <tara.kreft@gmail.com>
2015-11-23 23:34:51 -0800

    made server modular

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var http = require('http');
 
-function onRequest(req, res) {
+function start() {
+
+  function onRequest(req, res) {
 	console.log('request received.');
 	res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello World');
     res.end();
+  }
+  http.createServer(onRequest).listen(8888); 
+  console.log('server has started.');
 }
 
-http.createServer(onRequest).listen(8888); 
-
-console.log('server has started.');
+exports.start = start;
