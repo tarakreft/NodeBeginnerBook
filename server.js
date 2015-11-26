a975225d07c7aa55cf452de4418ae05a5d1a9e27
Tara Kreft <tara.kreft@gmail.com>
2015-11-25 18:11:59 -0800

    p.25 update of server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require('http');
+var url = require('url');
 
 function start() {
 
   function onRequest(req, res) {
-	console.log('request received.');
+  	var pathname = url.parse(req.url).pathname;
+	console.log('request for ' + pathname + ' received.');
 	res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello World');
     res.end();
