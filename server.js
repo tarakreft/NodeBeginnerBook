7fec7ac02c887a06ded1433f43fcff46cf295cf1
Tara Kreft <tara.kreft@gmail.com>
2015-11-25 18:56:28 -0800

    added router

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var http = require('http');
 var url = require('url');
 
-function start() {
+function start(route) {
 
   function onRequest(req, res) {
   	var pathname = url.parse(req.url).pathname;
 	console.log('request for ' + pathname + ' received.');
+
+  	route(pathname);
+
 	res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello World');
     res.end();
@@ -15,3 +18,4 @@ function start() {
 }
 
 exports.start = start;
+
