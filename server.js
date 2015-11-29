a66a04b5147e794c06934313e24e61bea1a238f9
Tara Kreft <tara.kreft@gmail.com>
2015-11-28 19:25:49 -0800

    added requestHandlers

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 var http = require('http');
 var url = require('url');
 
-function start(route) {
+function start(route, handle) {
 
   function onRequest(req, res) {
   	var pathname = url.parse(req.url).pathname;
 	console.log('request for ' + pathname + ' received.');
 
-  	route(pathname);
-
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World');
+	var content = route(handle, pathname)
+    res.write(content);
     res.end();
   }
   http.createServer(onRequest).listen(8888); 
