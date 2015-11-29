8d6d803af8ac63a4dc29d83ee74e71a29fcd7e81
Tara Kreft <tara.kreft@gmail.com>
2015-11-28 20:46:13 -0800

    fixed parenthesis

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,7 +3,7 @@ var exec = require('child_process').exec;
 function start(res) {
 	console.log('Request handler "start" was called.');
 
-	exec('find /') 
+	exec('find /', 
 	{ timeout: 10000, maxBuffer: 20000*1024 },
 	function (error, stdout, stderr) {
 		res.writeHead(200, {'Content-Type': 'text/plain'});
