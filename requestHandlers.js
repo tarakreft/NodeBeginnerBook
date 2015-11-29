f0e0e90ac4b1640e9e5183a9c17abd87a963435e
Tara Kreft <tara.kreft@gmail.com>
2015-11-28 20:44:04 -0800

    fixed misspelling of stdout, testing a more expensive operation

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,9 +3,11 @@ var exec = require('child_process').exec;
 function start(res) {
 	console.log('Request handler "start" was called.');
 
-	exec('ls -lah', function (error, stdout, stderr) {
+	exec('find /') 
+	{ timeout: 10000, maxBuffer: 20000*1024 },
+	function (error, stdout, stderr) {
 		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.write(stoudt);
+		res.write(stdout);
 		res.end();
 	});
 }
